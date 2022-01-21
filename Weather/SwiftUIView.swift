178649e28bebea5c7008e28c7109629833945216
Nathan Cohen <s015486@students.lmsd.org>
2022-01-21 12:32:25 -0500

    changes

--- Weather/SwiftUIView.swift
+++ Weather/SwiftUIView.swift
@@ -12,27 +12,45 @@ struct SwiftUIView: View {
     var hour : Hour
     
     var body: some View {
-        VStack{
-            if let test = hour.condition.icon{
-                KFImage(URL(string: "https:" + test))
-                Spacer()
-                
-                
-            }
-            else{
-                Image("download")
-                
-                
+        ZStack{
             
-                
-            }
-            Text(hour.time ?? "Time not Found" )
-            Text( "Temperature " + String(hour.temp_f ?? 0.0) +  " degrees")
-            Text( "Chance of Snow " + String(hour.chance_of_snow ?? 0.0)  + "%")
-            Text( "Chance of Rain " + String(hour.chance_of_rain ?? 0.0) + "%")
-            Text( "Real Feel " + String(hour.feelslike_f ?? 0.0))
+            Image("sky")
             Spacer()
+            VStack{
+                if let test = hour.condition.icon{
+                    KFImage(URL(string: "https:" + test))
+                        .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    
+                    
+                }
+                else{
+                 
+                    Image("download")
+                    
+                    
+                    
+                    
+                }
+                Text(hour.time ?? "Time not Found" )
+                    .font(.title)
+                    .multilineTextAlignment(.leading)
+                Text( "Temperature " + String(hour.temp_f ?? 0.0) +  " degrees F")
+                    .font(.title)
+                    .multilineTextAlignment(.leading)
+                Text( "Chance of Snow " + String(hour.chance_of_snow ?? 0.0)  + "%")
+                    .font(.title)
+                    .multilineTextAlignment(.leading)
+                Text( "Chance of Rain " + String(hour.chance_of_rain ?? 0.0) + "%")
+                    .font(.title)
+                    .multilineTextAlignment(.leading)
+                Text( "Real Feel " + String(hour.feelslike_f ?? 0.0) + "degrees F")
+                    .font(.title)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
         }
+        .edgesIgnoringSafeArea(.all)
         
     }
 }
@@ -40,5 +58,6 @@ struct SwiftUIView: View {
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
         SwiftUIView(hour:(Hour(condition: Condition(icon:"https://cdn.weatherapi.com/weather/64x64/night/116.png"), time: "0", temp_f: 0, chance_of_snow: 0, chance_of_rain: 0, feelslike_f: 0)))
+        
     }
 }
